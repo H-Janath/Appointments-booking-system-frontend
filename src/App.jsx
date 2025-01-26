71d189765e70d6d231f077f3d634540dd786e577
H-Janath <janathhma@gmail.com>
2025-01-26 07:20:04 +0530

    Add routing and new pages for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointment from "./pages/MyAppointment";
+import Appointmet from "./pages/Appointment";  
 
 const App = () => {
   return (
-    <div>
-         </div>
-  )
-}
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors/>} />
+        <Route path="/doctors/:specialicity" element={<Doctors/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/my-profile" element={<MyProfile/>} />
+        <Route path="/my-appointment" element={<MyAppointment/>} />
+        <Route path="/appointent/:docId" element={<Appointmet/>} />
 
-export default App
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
