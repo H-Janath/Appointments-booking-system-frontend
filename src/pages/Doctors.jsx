63d829d66eaa6d8024ec8bbd1fc79ad6beccaeb7
H-Janath <janathhma@gmail.com>
2025-01-29 06:54:47 +0530

    Add mobile navigation menu and filter functionality in Doctors page

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -4,6 +4,7 @@ import { AppContext } from "../context/AppContext";
 const Doctors = () => {
   const { specialicity } = useParams();
   const [filterDoctors, setFilterDoctors] = useState([]);
+  const [showFilter,setShowFilter] = useState(false)
   const { doctors } = useContext(AppContext);
   const navigate = useNavigate();
 
@@ -23,7 +24,8 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button className={`py-1 px-3 border rounded text-sm transtion-all sm:hidden ${showFilter?'bg-sky-700 text-white ':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filter</button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter?'flex':'hidden sm:flex'}`}>
           <p onClick={()=>specialicity==='General physician'? navigate('/doctors'):navigate(`/doctors/General physician`)} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialicity ==="General physician"?"bg-indigo-100 text-black":""}`}>General physician</p>
           <p onClick={()=>specialicity==='Gynecologist'? navigate('/doctors'):navigate(`/doctors/Gynecologist`)} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialicity ==="Gynecologist"?"bg-indigo-100 text-black":""}`}>Gynecologist</p>
           <p onClick={()=>specialicity==='Dermatologist'? navigate('/doctors'):navigate(`/doctors/Dermatologist`)} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialicity ==="Dermatologist"?"bg-indigo-100 text-black":""}`}>Dermatologist</p>
